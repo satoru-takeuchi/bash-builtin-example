709a90e8ff7ea3561a423b2bda1ef5d9a3093699
Satoru Takeuchi <satoru.takeuchi@gmail.com>
2017-01-17 22:30:06 +0900

    Make it work

---
+++ myhello.c
@@ -0,0 +1,26 @@
+#include <builtins.h>
+#include <shell.h>
+#include <stdio.h>
+
+static int myhello(WORD_LIST *list) {
+	puts("Hello world!");
+	fflush(stdout);
+	return EXECUTION_SUCCESS;
+}
+
+static char *desc[] = {
+	"Show a greeting message.",
+	"",
+	"It's far faster than launching executable file",
+	"because it't not necessary to exec() and fork().",
+	(char *)NULL
+};
+
+struct builtin myhello_struct = {
+	"myhello",		// builtin command name
+	myhello,		// function called when issueing this command
+	BUILTIN_ENABLED,	// initial flag
+	desc,			// long description
+	"hello",		// short description
+	0,
+};
